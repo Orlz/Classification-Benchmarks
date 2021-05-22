4d331f234f7acf573e7df7ae576948c8e5c58624
Orla Mallon <52678852+Orlz@users.noreply.github.com>
2021-05-22 08:33:30 +0200

    Update create_virtual_environment.sh
    Undoing bug fix from yesterday as original file was fine

--- create_virtual_environment.sh
+++ create_virtual_environment.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-VENVNAME= Computer_Vision02 
+VENVNAME=Computer_Vision02
 
 python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 pip install --upgrade pip
 
-# problems when installing from requirements.txt
 pip install ipython
 pip install jupyter
-pip install networkx
-pip install sklearn
+pip install pandas
 
-python -m ipykernel install --user --name=$VENVNAME
+#python -m ipykernel install --user --name=$VENVNAME
 
-test -f requirements.txt && pip install requirements.txt
+test -f requirements.txt && pip install -r requirements.txt
 
 deactivate
 echo "build $VENVNAME"
